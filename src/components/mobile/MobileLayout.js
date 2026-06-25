f5e8bafa86cb87e807d61df9c1cefe558f60a27a
Arman Akhtar <armanakhtar297@gmail.com>
2026-06-25 12:00:04 +0530

    mob

--- src/components/mobile/MobileLayout.js
+++ src/components/mobile/MobileLayout.js
@@ -22,7 +22,7 @@ const TABS = [
 
 export default function MobileLayout() {
   const { state, openApp } = useOs();
-  const [tab, setTab] = useState('resume');
+  const [tab, setTab] = useState('about');
 
   const terminalWindow = state.windows.find((w) => w.appId === 'terminal');
 
